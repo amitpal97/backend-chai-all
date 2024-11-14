6939fa521666b8f1d04c096abe9d6fad399b143b
Amit Pal <Amit.Pal@teknopoint.local>
2024-11-14 18:34:16 +0530

    mongose db connected

---
+++ chapter2-project/src/db/index.js
@@ -0,0 +1,19 @@
+import mongoose from "mongoose"
+import { DB_NAME } from "../constants.js"
+
+const connectDB = async () => {
+    try {
+        const ConnectionInstance = await (mongoose.connect(`${process.env.MONGODB_URI}/${DB_NAME}`))
+        console.log("ConnectionInstance for Knowlewge", ConnectionInstance);
+        console.log(`\n MongoDB Connected !! DB Host :${ConnectionInstance.connection.host}`);
+
+    }
+    catch (error) {
+        console.log("MongoDB connectionError", error);
+        process.exit(1)
+
+    }
+}
+
+
+export default connectDB
