03612a536c17a9d565de2a255b302462eaa75854
Amit Pal <Amit.Pal@teknopoint.local>
2024-11-14 19:09:01 +0530

    adding cross and cookie -browser

--- chapter2-project/src/db/index.js
+++ chapter2-project/src/db/index.js
@@ -4,7 +4,7 @@ import { DB_NAME } from "../constants.js"
 const connectDB = async () => {
     try {
         const ConnectionInstance = await (mongoose.connect(`${process.env.MONGODB_URI}/${DB_NAME}`))
-        console.log("ConnectionInstance for Knowlewge", ConnectionInstance);
+        // console.log("ConnectionInstance for Knowlewge", ConnectionInstance);
         console.log(`\n MongoDB Connected !! DB Host :${ConnectionInstance.connection.host}`);
 
     }
--- chapter2-project/src/index.js
+++ chapter2-project/src/index.js
@@ -1,14 +1,27 @@
 
 import dotenv from "dotenv";
 import connectDB from "./db/index.js"
+import {app} from "./app.js"
 
 
 dotenv.config({
-    path:"./env"
+    path: "./env"
 })
 
 
-connectDB()
+connectDB().then(() => {
+    app.on("error", (error) => {
+        console.log(" Server Error", error);
+    })
+    app.listen(process.env.PORT || 8000, () => {
+        console.log(`servers is running on port ${process.env.PORT || 8000}`);
+
+    })
+})
+    .catch((error) => {
+        console.log("MOngo Db connection failed !!!");
+
+    })
 
 
 
